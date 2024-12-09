e890133023c3db9d8c9b4c1a30d6dfde276016c3
Priyanka Soni <140409655+priyankasoni930@users.noreply.github.com>
2024-12-09 10:53:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { YoutubeTranscript } = require("youtube-transcript");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3004;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -17,13 +17,18 @@ app.get("/transcript", async (req, res) => {
   const videoId = req.query.videoId;
 
   if (!videoId) {
+    console.log("checking video id");
     return res.status(400).json({ error: "Video ID is required" });
   }
 
   try {
+    console.log("before fetching transcript");
     const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+    console.log("after fetching transcript");
     res.json(transcript);
+    console.log("after sending response");
   } catch (error) {
+    console.log("if got error");
     res.status(500).json({ error: error.message });
   }
 });
